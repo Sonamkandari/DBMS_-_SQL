59ea77a54a57d3264f9b26f974a8d03ffefcd885
Sonam Kandari <145662567+Sonamkandari@users.noreply.github.com>
2026-06-08 13:40:16 +0530

    Update chater_4.sql

--- Practice_work/Datacamp_Practice_questions/chater_4.sql
+++ Practice_work/Datacamp_Practice_questions/chater_4.sql
@@ -109,6 +109,17 @@ select name
 from countries 
 ORDER BY name;
 
+-- Query the right table in information_schema to get columns
+SELECT column_name, data_type 
+FROM information_schema.columns
+WHERE table_name = 'university_professors' AND table_schema = 'public';
 
+-- Rename the organisation column
+ALTER TABLE affiliations
+RENAME COLUMN organisation TO organization;
+
+-- Delete the university_shortname column
+ALTER TABLE affiliations
+DROP COLUMN university_shortname;
 
 
